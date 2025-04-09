b1743929dbcba53ed6c4c7ac6a4831563bb6fa93
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2025-04-09 13:29:39 +0200

    minimal lsp and formatter setup

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				bash = { "shfmt" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				r = { "air" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+	{
+		"zapling/mason-conform.nvim",
+		opts = {},
+	},
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"r_language_server",
+				"marksman",
+				"rust_analyzer",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").lua_ls.setup({})
+			require("lspconfig").pyright.setup({})
+			require("lspconfig").r_language_server.setup({})
+			require("lspconfig").marksman.setup({})
+			require("lspconfig").rust_analyzer.setup({})
+		end,
+	},
+}
